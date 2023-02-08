51e748a5eee5e88ba2055ad8f7c1228a971323f1
thesevenq <59538879+thesevenq@users.noreply.github.com>
2023-02-08 23:38:20 +0100

    integration(Territories): added web/ui files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,11 +8,20 @@ description 'Territory System for Gangs'
 
 version 'v2.0.1'
 
+ui_page "web/index.html"
+
 shared_scripts {
   '@es_extended/imports.lua',
   '@ox_lib/init.lua',
   'shared.lua'
 }
+
+files {
+  "web/index.html",
+  "web/script.js",
+  "web/style.css",
+}
+
 
 client_script 'client.lua'
 
