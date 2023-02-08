abbc8e39d9b700c82109a28c6b443a2bdca3445a
Tomić <tomiichx@gmail.com>
2023-02-09 00:02:57 +0100

    refactor(UI): use jQuery

--- web/script.js
+++ web/script.js
@@ -1,15 +1,21 @@
-console.log('debug')
-window.addEventListener('message', function(event) {
-	let data = event.data
-	if (data.akcija == "pokazi") {
-		$("#scoreboard").show()
-		$("#ime-teritorije").html(data.ime_teritorije)
-		$("#scoreboard-session").html('CAPTURING IN PROGRESS')
-		$("#scoreboard-attack").html(data.broj_napadaca)
-		$("#scoreboard-defend").html(data.broj_defendera)
-
-
-	} else if (data.akcija == "sakrij") {
-		$("#scoreboard").hide()
+$(window).on("message", function (event) {
+	const data = event.data;
+	switch (data.request) {
+		case "show":
+			$("#scoreboard").show()
+				.find("#scoreboard-territory").html(data.territory_name).end()
+				.find("#scoreboard-status").html(data.territory_status).end()
+				.find("#scoreboard-attackers_count").html(data.territory_attackers_count).end()
+				.find("#scoreboard-defenders_count").html(data.territory_defenders_count);
+			break;
+		case "update":
+			$("#scoreboard")
+				.find("#scoreboard-status").html(data.territory_status).end()
+				.find("#scoreboard-attackers_count").html(data.territory_attackers_count).end()
+				.find("#scoreboard-defenders_count").html(data.territory_defenders_count);
+			break;
+		case "hide":
+			$("#scoreboard").hide();
+			break;
 	}
 });
