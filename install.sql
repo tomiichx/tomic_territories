5b10a18182fc1ceab318774fa8b0f7b9f1d9cda3
Tomić <tomiichx@gmail.com>
2025-03-06 21:52:31 +0100

    fix(SQL): invalid syntax

--- install.sql
+++ install.sql
@@ -1,15 +1,13 @@
--- Fresh install in case this script hasn't been used previously on your server.
-
 CREATE TABLE IF NOT EXISTS `tomic_territories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text DEFAULT 'NewTerritory',
-  `owner` text DEFAULT 'noone',
-  `label` text DEFAULT 'NoOne',
-  `radius` int(11) DEFAULT 50,
-  `coords` longtext NOT NULL,
-  `type` text DEFAULT 'default',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) DEFAULT 'NewTerritory',
+  `owner` VARCHAR(255) DEFAULT 'noone',
+  `label` VARCHAR(255) DEFAULT 'NoOne',
+  `radius` INT(11) DEFAULT 50,
+  `coords` VARCHAR(255) DEFAULT '{"x":0,"y":0,"z":0}',
+  `type` VARCHAR(255) DEFAULT 'default',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 INSERT IGNORE INTO `tomic_territories` (`id`, `name`, `owner`, `label`, `radius`, `coords`, `type`) VALUES
 	(1, 'Motel', 'gsf', 'GSF', 60, '{"z":33.239501953125,"y":3574.15380859375,"x":1567.4241943359376}', 'dealer'),
--- update.sql
+++
@@ -1,6 +0,0 @@
--- Execute this to update the columns if you already have used the script before.
-
-ALTER TABLE `jobs`
-  CHANGE COLUMN `nedpoeni` `weeklyPoints` INT(11) DEFAULT 0,
-  CHANGE COLUMN `mespoeni` `monthlyPoints` INT(11) DEFAULT 0,
-  CHANGE COLUMN `poeni` `totalPoints` INT(11) DEFAULT 0;
